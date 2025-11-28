2e117c6e30e527c82e5ecebcfea74d9da74e481b
Denis Salnikov <denissalnikov8@gmail.com>
2025-11-28 18:02:16 +0600

    chore: change app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,9 +1,29 @@
 import streamlit as st
 import requests
 
-API_URL = "http://127.0.0.1:8000/generate" 
+st.title("ИИ ассистент по коституции РФ")
 
-st.title("LLM Chat (Streamlit + FastAPI)")
+
+st.subheader("Настройки подключения")
+
+ip_option = st.selectbox(
+    "Выберите адрес сервера:",
+    ["localhost (127.0.0.1)", "PC (192.168.0.245)", "Ввести вручную"]
+)
+
+if ip_option == "localhost (127.0.0.1)":
+    selected_ip = "127.0.0.1"
+
+elif ip_option == "PC (192.168.0.245)":
+    selected_ip = "192.168.0.245"
+
+else:
+    selected_ip = st.text_input("Введите IP вручную:", value="192.168.0.245")
+
+API_URL = f"http://{selected_ip}:8000/generate"
+
+
+st.subheader("Чат с моделью")
 
 query = st.text_input("Введите ваш вопрос:")
 
@@ -11,15 +31,15 @@ if st.button("Отправить"):
     if query.strip() == "":
         st.warning("Введите вопрос!")
     else:
-        try:
-            response = requests.post(
-                API_URL,
-                json={"query": query}
-            )
-            if response.status_code == 200:
-                answer = response.json().get("answer")
-                st.success(answer)
-            else:
-                st.error(f"Ошибка сервера: {response.status_code}")
-        except Exception as e:
-            st.error(f"Ошибка запроса: {e}")
+        with st.spinner("Генерация ответа..."):
+            try:
+                response = requests.post(API_URL, json={"query": query})
+                
+                if response.status_code == 200:
+                    answer = response.json().get("answer")
+                    st.success(answer)
+                else:
+                    st.error(f"Ошибка сервера: {response.status_code}")
+
+            except Exception as e:
+                st.error(f"Ошибка запроса: {e}")
